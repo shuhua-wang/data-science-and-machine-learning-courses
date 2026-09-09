a14004fcaf38dcebd9bab84b8fe2b415932bb8da
Shuhua Wang <shuhua.ws@gmail.com>
2026-09-09 04:28:25 +0000

    add dev container

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# data-science-and-machine-learning-courses
+# Data Science and Machine Learning Courses
+
+## Tutorial
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from data-science-and-machine-learning-courses!")
+
+
+if __name__ == "__main__":
+    main()
